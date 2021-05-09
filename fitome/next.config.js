82c3720297b83ff132103778d5303f1fd2de52a0
Louisa <louisawong95@hotmail.com>
2021-05-09 16:31:42 -0400

    setup next and next-pwa install

---
+++ fitome/next.config.js
@@ -0,0 +1,20 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+        net: 'empty',
+      }
+    }
+
+    return config
+  }
+})
