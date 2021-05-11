fb103e64b3ca6e2198f40c8c108d0cabf081b158
Louisa <louisawong95@hotmail.com>
2021-05-11 19:26:13 -0400

    fix/soxket connection with peerjs

--- fitome/pages/videoRoom/[sessionId].js
+++ fitome/pages/videoRoom/[sessionId].js
@@ -22,51 +22,104 @@
 // export default videoRoom
 
 import React, {useEffect, useState, useRef} from 'react'
-import Peer from 'peerjs';
+// import Peer from 'peerjs';
 import { useRouter } from 'next/router'
+import {useAuth} from '../../firebase/contextAuth'
+import {socket} from '../../lib/socket'
 
 function VideoRoom() {
-    const myPeer = new Peer(); // create peer element for current user
     const router = useRouter();
-    const { id } = router.query;
+    const { currentUser } = useAuth();
     const [stream, setStream] = useState(null)
     const videoRef = useRef();
     const videoRef2 = useRef();
-
-    useEffect(() => {
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        }). then (stream => {
-            console.log(stream);
-            videoRef.current.srcObject = stream;
-            
-            myPeer.on('call', call => { // When we join someone's room we will receive a call from them
-                call.answer(stream) // Stream them our video/audio
-                call.on('stream', userVideoStream => { // When we recieve their stream
-                    videoRef.current.srcObject = userVideoStream; // Display their video to ourselves
-                })
-            })
-
-            socket.on('joinRoom', userId => { // If a new user connect
-                connectToNewUser(userId, stream) 
-            })
-            
+    const { sessionId } = router.query;
+    // const myPeer = new Peer(trainer_uid, {
+    //     host: "localhost",
+    //     path: "/peerjs",
+    //     port: 3001
+    // }); // create peer element for current user
+    
+    function connectToNewUser(peerId, stream, myPeer) { // This runs when someone joins our room
+        console.log("CONNECTING TO NEW USER", peerId, " ", stream, myPeer)
+        const call = myPeer.call(peerId, stream) // Call the user who just joined
+        //Add their video
+        console.log("CALLING", call)
+        call.on("stream", userVideoStream => {
+            console.log("OTHER STREAM")
+            videoRef2.current.srcObject = userVideoStream;
         })
-    },[])
+        // If they leave, remove their video
+        // call.on('close', () => {
+        //     videoRef2.remove()
+        // })
+    }
+    
+    useEffect(() => {
+        if (currentUser) {
+            import('peerjs').then(({ default: Peer }) => {
+                const myPeer = new Peer(currentUser.uid, {
+                    host: "localhost",
+                    path: "/peerjs",
+                    port: 3001
+                });
+                navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: true
+                }). then (stream => {
+                    console.log(stream);
+                    videoRef.current.srcObject = stream;
+                    
+                    myPeer.on('call', call => { // When we join someone's room we will receive a call from them
+                        console.log("ANSWERING CALL", call)
+                        call.answer(stream) // Stream them our video/audio
+                        call.on("stream", userVideoStream => { // When we recieve their stream
+                            videoRef2.current.srcObject = userVideoStream; // Display their video to ourselves
+                        })
+                    })
+                    
+
+                    socket.on('user-connected', userId => { // If a new user connect, connect to them
+                        console.log("Socket user connected", userId);
+                        connectToNewUser(userId, stream, myPeer) 
+                    })
+                    
+                })
+                
+                myPeer.on('open', peerId => { // When we first open the app, have us join a room
+                    socket.emit('join-room', {userId: peerId, firstName:"MARK", sessionId:sessionId})
+                    console.log("PEERID", peerId, " ROOMID ", sessionId)
+                })
+            });
+        }
+    },[currentUser])
+    
+   
+
     return (
         <div>
             <video autoPlay
-            ref={videoRef}
-            className="video_me" 
-            muted={true}></video>
+                ref={videoRef}
+                className="video_me" 
+                muted={true}>
+            </video>
             <video autoPlay
-            ref={videoRef2}
-            className="video_them" 
-            muted={true}></video>
+                ref={videoRef2}
+                className="video_them" 
+                muted={true}>
+            </video>
         </div>
     )
 }
+
+// export async function getServerSideProps(context) {
+//     //api call
+//     return {
+//       props: {
+//           trainer_uid: "123456"
+//       }, // will be passed to the page component as props
+//     }
+//   }
 
 export default VideoRoom
 
--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const io = require('socket.io')(server, {
     origin: "http://localhost:3000"
   }
 })
-
+const { ExpressPeerServer } = require('peer');
 const { v4: uuidV4 } = require('uuid')
 const mocks = require('./mocks')
+const peerServer = ExpressPeerServer(server, {
+  path: '/'
+});
 
+const rooms = {};
+
+app.use('/peerjs', peerServer);
 // app.use(cors());
 
 // app.get('/', (req, res) => {
@@ -27,7 +33,10 @@ const mocks = require('./mocks')
 // app.get('/video/:room', (req, res) => {
 //   res.send(req.params.room);
 // });
-
+peerServer.on('connection', peer => {
+  console.log("Peer connected", peer.id)
+  // peerServer.socket
+})
 
 io.on('connection', socket => {
   console.log('Connected to Client')
@@ -42,23 +51,29 @@ io.on('connection', socket => {
 
   //listens for a useEffect to check get sessionId row and send it back to page session/:id
   socket.on('isRoomActive', ({userId, sessionId}, callback) => {
-    //TOOD: call controller to get appointment row and send the whole row back
+    //TODO: call controller to get appointment row and send the whole row back
     callback(mocks.withRoom)
   });
 
   //listens for join an existing room given by roomId from "JOIN CALL" button on page session/:id
   socket.on('joinRoom', ({userId, firstName, sessionId}) => {
     socket.join(sessionId)  //join existing room
+    rooms[sessionId] = { guest: userId };
     let numClients = io.sockets.adapter.rooms.get(sessionId).size
     console.log ("JOINING:", numClients)
     //let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
     //console.log("JOINING ROOM", sessionId, " with ", numClients, " ppl")
     
-    socket.broadcase.emit("user-connected", userId) //to tell person in room you are there
+    socket.to(sessionId).emit("user-connected", userId)
+    // socket.broadcast.emit("user-connected", userId) //to tell person in room you are there
+
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', userId)
+  })
   })
 
   //listens for a disconnect
-  socket.on('disconnect', () => {
+  socket.on('hang-up', () => {
     socket.broadcast.emit("Call Ended")
   })
 });
