cbc210fa6c8c0d353c1d7a505981c926de8ee226
Massimo Bergamin <berg.massimo@gmail.com>
2021-05-13 00:51:12 -0400

    profile update form

---
+++ fitome/pages/profile.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useAuth } from '../firebase/contextAuth'
+import { useSelector } from 'react-redux';
+import UpdateProfileForm from '../components/updateProfileForm';
+import { updateUser } from '../redux/trainer';
+import { useDispatch } from 'react-redux';
+
+const Profile = () => {
+  
+  const { user } = useSelector(state => state.trainer);
+
+
+
+  return (
+    <div>
+      <p>{user.first_name + " " + user.last_name}</p>
+      <UpdateProfileForm></UpdateProfileForm>
+    </div>
+  )
+}
+
+export default Profile;
