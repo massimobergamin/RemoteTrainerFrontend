ce48da3615a45ab24aa1d32815b60396af21cfff
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-05-12 17:59:28 -0700

    Update exercise.js

--- fitome/pages/exercise.js
+++ fitome/pages/exercise.js
@@ -3,6 +3,7 @@ import { postExercise } from '../redux/trainer';
 import { useAuth } from '../firebase/contextAuth';
 import { useDispatch } from 'react-redux';
 import { router } from 'next/router';
+import { useState } from 'react';
 
 const Exercise = () => {
   const [media, setMedia] = useState('');
