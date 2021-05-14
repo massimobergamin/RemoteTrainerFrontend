e81102144e124095a8447bc97bf3f386cd916c7d
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-05-14 16:00:42 -0700

    workouts and exercises now working

--- fitome/pages/trainer/workouts/create.js
+++ fitome/pages/trainer/workouts/create.js
@@ -11,32 +11,40 @@ const CreateWorkout = () => {
     exerciseIds: []
   };
   const [formState, setFormState] = useState(initialState);
-  const [buttonText, setButtonText] = useState('Add');
+  const [buttonTextArr, setButtonTextArr] = useState([]);
   const { currentUser } = useAuth();
   const dispatch = useDispatch();
   const { exercises } = useSelector(state => state.trainer);
 
   useEffect(() => {
+    console.log('currentUser', currentUser);
+    console.log('exercises', exercises)
     dispatch(getExercise(currentUser.uid));
   }, [])
 
-  const handleClick = (e, id) => {
-    if (buttonText === 'Add') {
-      setFormState({...formState, exercisesIds: [...formState.exerciseIds, id]});
-      setButtonText('Remove');
+  const handleClick = (id, i) => {
+    if (buttonTextArr[i] === 'Add' || !buttonTextArr[i]) {
+      setFormState({...formState, exerciseIds: [...formState.exerciseIds, id]});
+      let tempArr = buttonTextArr;
+      tempArr[i] = 'Remove';
+      setButtonTextArr(tempArr);
+
     }
     else {
       let tempExerciseIds = formState.exerciseIds;
       tempExerciseIds = tempExerciseIds.filter(exerciseId => exerciseId !== id);
-      setFormState({...formState, exercisesIds: [...tempExerciseIds]});
-      setButtonText('Add');
+      setFormState({...formState, exerciseIds: [...tempExerciseIds]});
+      let tempArr = buttonTextArr;
+      tempArr[i] = 'Add';
+      setButtonTextArr(tempArr);
     }
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     try {
       dispatch(postWorkout({trainer_uid: currentUser.uid, workoutData: formState}));
-      router.push('/workouts');
+      router.push('/trainer/workouts');
     } catch (err) {
       console.log(err);
     }
@@ -47,21 +55,20 @@ const CreateWorkout = () => {
       <h1>Create New Workout</h1>
       <form>
           <input placeholder="Title" type="text" onChange={(e) => setFormState({...formState, title: e.target.value})}/>
-          <input type="submit" value="Create" onClick={handleSubmit}/>
-          <h3>Select Exercises</h3>
-          {exercises.map(exercise => {
-            <div key={exercise.id}>
-              <div>{exercise.title}</div>
-              {exercise.media &&
-                <video id="Exercise_Video" width="176" height="176" autoplay="true" loop="true">
-                    <source src={exercise.media} type="video/mp4"/>
-                    Your browser does not support HTML5 video.
-                </video>}
-              <div>{exercise.muscle_group}</div>
-              <button onClick={() => handleClick(e, exercise.id)}>{buttonText}</button>
-            </div>
-          })}
+          <button onClick={handleSubmit}>Create</button>
       </form>
+        <h3>Select Exercises</h3>
+        {exercises && exercises.map((exercise, i) =>
+          <div key={exercise.id}>
+            <div>{exercise.title}</div>
+            {exercise.media &&
+              <video id="Exercise_Video" width="176" height="176" autoplay="true" loop="true">
+                  <source src={exercise.media} type="video/mp4"/>
+                  Your browser does not support HTML5 video.
+              </video>}
+            <div>{exercise.muscle_group}</div>
+            <button onClick={() => handleClick(exercise.id, i)}>{buttonTextArr[i] || 'Add'}</button>
+          </div>)}
     </div>
   )
 }
