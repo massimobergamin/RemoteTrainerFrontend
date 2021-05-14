4e965b294da15c9727d20f8d83775fadddfae6a2
Louisa <louisawong95@hotmail.com>
2021-05-14 09:14:56 -0400

    trying to merge functioning video call

--- fitome/pages/session/[id].js
+++ fitome/pages/session/[id].js
@@ -12,41 +12,42 @@ function SessionDetailPage () {
     const { currentUser } = useAuth();
     const [session, setSession] = useState(null);
 
-    useEffect(() => {
-        socket.emit('isRoomActive', {userId:currentUser.uid, sessionId: id}, (response) => {
-            console.log(response)
-            setSession(response)
-        })
-    }, [router])
+    // useEffect(() => {
+    //     socket.emit('isRoomActive', {userId:currentUser.uid, sessionId: id}, (response) => {
+    //         console.log(response)
+    //         setSession(response)
+    //     })
+    // }, [router])
 
     function joinCallHandler () {
         console.log("joining")
         //emit
-        socket.emit('joinRoom', {userId: currentUser.uid, firstName: "MARK", roomId: session.id}); //get firstName from redux
+        //socket.emit('joinRoom', {userId: currentUser.uid, firstName: "MARK", roomId: session.id}); //get firstName from redux
         router.push(`/videoRoom/${id}`)
     }
 
-    function startCallHandler () {
-        console.log("starting")
-        //emit
-        socket.emit('createNewRoom', {userId: currentUser.uid, firstName: "MARK"}); //get firstName from redux
-        router.push(`/videoRoom/${id}`);
-    }
+    // function startCallHandler () {
+    //     console.log("starting")
+    //     //emit
+    //     socket.emit('createNewRoom', {userId: currentUser.uid, firstName: "MARK"}); //get firstName from redux
+    //     router.push(`/videoRoom/${id}`);
+    // }
 
-    function showButton () {
-        if (session?.in_use===true) {
-            return <button className="button" type="button" onClick={joinCallHandler}>JOIN CALL</button>
-        } else {
-            return <button className="button" type="button" onClick={startCallHandler}>START CALL</button>
-        }
-    }
+    // function showButton () {
+    //     if (session?.in_use===true) {
+    //         return <button className="button" type="button" onClick={joinCallHandler}>JOIN CALL</button>
+    //     } else {
+    //         return <button className="button" type="button" onClick={startCallHandler}>START CALL</button>
+    //     }
+    // }
 
     return (
         <div>
             <h1>Your Next Training Session:</h1>
-            <h3>Session with <span>{session?.client_uid}</span></h3>
+            {/* <h3>Session with <span>{session?.client_uid}</span></h3>
             <p>{Moment(session?.startDate).format('LLL')}</p>
-            {showButton()}
+            {showButton()} */}
+            <button className="button" type="button" onClick={joinCallHandler}>JOIN CALL</button>
         </div>
     )
 }
