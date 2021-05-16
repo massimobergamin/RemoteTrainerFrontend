f94203d48a499b60d02052713ecd2e066dfca4f6
Louisa <louisawong95@hotmail.com>
2021-05-16 13:34:36 -0400

    added redux-persist

--- fitome/redux/store.js
+++ fitome/redux/store.js
@@ -1,10 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit'
 import trainerReducer from './trainer'
 import clientReducer from './client'
+import storage from 'redux-persist/lib/storage'
+import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist'
+import thunk from 'redux-thunk'
 
-export default configureStore({
-  reducer: {
-    trainer: trainerReducer,
-    client: clientReducer,
-  },
+
+const reducers = combineReducers({
+  trainer: trainerReducer,
+  client: clientReducer,
 })
+
+const persistConfig = {
+  key:'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware:[thunk],
+})
+
+export default store;
+// export default configureStore({
+//   reducer: {
+//     trainer: trainerReducer,
+//     client: clientReducer,
+//   },
+// })
