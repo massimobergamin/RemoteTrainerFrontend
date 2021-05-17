47969d27fa72c6cc83bde011c8ef81324e42b01d
Louisa <louisawong95@hotmail.com>
2021-05-16 23:02:22 -0400

    small edits in signup and css

--- fitome/pages/signup.js
+++ fitome/pages/signup.js
@@ -12,7 +12,7 @@ const SignUp = () => {
     const router = useRouter();
 
     const initialState = {
-        user_uid: '',
+        user_uid: "",
         first_name: "",
         last_name: "",
         email: "",
@@ -23,30 +23,30 @@ const SignUp = () => {
     }
 
     const inviteInitialState = {
-        user_uid: '',
-        invite_code: ''
+        user_uid: "",
+        invite_code: ""
     }
-    const [inviteState, setInviteState] = useState(inviteInitialState)
 
+    const [inviteState, setInviteState] = useState(inviteInitialState)
     const [formState, setFormState] = useState(initialState);
     const { user, invite_code } = useSelector(state => state.trainer);
-    // console.log("outside function", user)
-    // console.log("invitecode", invite_code)
+
+    function titleCase(name){
+        return name[0].toUpperCase() + name.slice(1).toLowerCase();
+    }
+
     const createHandler = async () => {
-       
         try {
-          const fireBaseData = await signUp(formState.email, formState.password, formState.type);
-            //console.log(fireBaseData.user.uid)
-        //   setFormState({...formState, user_uid:fireBaseData.user.uid, last_login: Date.now()})
-        //   setInviteState({...inviteState, user_uid:fireBaseData.user.uid, invite_code: nanoid(5)})
-        
-          if (formState.type === 'trainer') {
-    
-              await dispatch(postUser({...formState, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
-              await dispatch(postInviteCode({...inviteState, user_uid:fireBaseData.user.uid, invite_code: nanoid(5)}));
-              router.push(`/trainer/invitecode`);
+            const fireBaseData = await signUp(formState.email, formState.password, formState.type);
+            let lowerType = formState.type.toLowerCase();
+            let firstName = titleCase(formState.first_name);
+            let lastName = titleCase(formState.last_name);
+            if (formState.type === 'trainer') {
+                await dispatch(postUser({...formState, type: lowerType, first_name: firstName, last_name: lastName, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
+                await dispatch(postInviteCode({...inviteState, user_uid:fireBaseData.user.uid, invite_code: nanoid(5).toUpperCase()}));
+                router.push(`/trainer/invitecode`);
             } else {
-              await dispatch(postUser({...formState, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
+              await dispatch(postUser({...formState, type: lowerType, first_name: firstName, last_name: lastName, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
               router.push(`/client/trainercode`);
             }
         } catch (err) {
@@ -57,32 +57,48 @@ const SignUp = () => {
     return (
         <div>
             <form>
+                <label className="sessionCreate_field" htmlFor="firstName">First Name:</label>
                 <input type="text" 
                     placeholder="First Name"
+                    name="firstName"
                     value={formState.firstName} 
                     onChange={(e)=>setFormState({...formState, first_name:e.target.value})}/>
+                <label className="sessionCreate_field" htmlFor="lastName">Last Name:</label>
                 <input type="text" 
+                    name="lastName"
                     placeholder="Last Name"
                     value={formState.lastName} 
                     onChange={(e)=>setFormState({...formState, last_name:e.target.value})}/>
+                <label className="sessionCreate_field" htmlFor="username">Account Username:</label>
                 <input type="text" 
+                    name="username"
                     placeholder="Username"
                     value={formState.username} 
                     onChange={(e)=>setFormState({...formState, username:e.target.value})}/>
+                <label className="sessionCreate_field" htmlFor="email">Email:</label>
                 <input type="email"
+                    name="email"
                     placeholder="Email"
                     value={formState.email} 
                     onChange={(e)=>setFormState({...formState, email:e.target.value})}/>
+                <label className="sessionCreate_field" htmlFor="password">Password:</label>
                 <input type="password"
+                    name="password"
                     placeholder="Password"
                     value={formState.password} 
                     onChange={(e)=>setFormState({...formState, password:e.target.value})}/>
-                <input type="type"
-                    placeholder="Type"
-                    value={formState.type} 
+                <label className="sessionCreate_field" htmlFor="type">Select an Account Type:</label>
+                <input list="accountType"
+                    id="listoftype"
+                    name="listoftype"
+                    placeholder="Account Type"
                     onChange={(e)=>setFormState({...formState, type:e.target.value})}/>
+                <datalist id="accountType">
+                    <option value="Client"></option>
+                    <option value="Trainer"></option>
+                </datalist>
                 <button type="button" 
-                    disabled={formState.password===""||formState.email===""||formState.first_name===""||formState.last_name===""||formState.username===""}
+                    disabled={formState.password===""||formState.email===""||formState.first_name===""||formState.last_name===""||formState.username===""||formState.type===""}
                     onClick={createHandler}>SIGN UP</button>
             </form>
             <p>Already have an account? <Link href="/"><a><span>Login.</span></a></Link></p>
