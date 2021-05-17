de3935fed8e954a79ea94b97348264d638bab28c
ParksDivision <KyleJohnParks@gmail.com>
2021-05-17 12:30:34 -0500

    bug fixes2

--- fitome/pages/signup.js
+++ fitome/pages/signup.js
@@ -37,17 +37,17 @@ const SignUp = () => {
 
     const createHandler = async () => {
         try {
-            const fireBaseData = await signUp(formState.email, formState.password, formState.type);
             let lowerType = formState.type.toLowerCase();
             let firstName = titleCase(formState.first_name);
             let lastName = titleCase(formState.last_name);
-            if (formState.type === 'Trainer') {
+            const fireBaseData = await signUp(formState.email, formState.password, lowerType);
+            if (formState.type === 'trainer') {
                 await dispatch(postUser({...formState, type: lowerType, first_name: firstName, last_name: lastName, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
                 await dispatch(postInviteCode({...inviteState, user_uid:fireBaseData.user.uid, invite_code: nanoid(5).toUpperCase()}));
                 router.push(`/trainer/invitecode`);
             } else {
               await dispatch(postUser({...formState, type: lowerType, first_name: firstName, last_name: lastName, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
-              router.push(`/client/trainercode`);
+              router.push(`/client/invitecode`);
             }
         } catch (err) {
             console.error(err)
