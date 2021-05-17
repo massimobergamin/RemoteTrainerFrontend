4422df317958f7fcf3ff54ef8508223b449e7c2f
Louisa <louisawong95@hotmail.com>
2021-05-17 13:35:31 -0400

    trying to merge

--- fitome/pages/signup.js
+++ fitome/pages/signup.js
@@ -37,10 +37,10 @@ const SignUp = () => {
 
     const createHandler = async () => {
         try {
-            const fireBaseData = await signUp(formState.email, formState.password, formState.type);
             let lowerType = formState.type.toLowerCase();
             let firstName = titleCase(formState.first_name);
             let lastName = titleCase(formState.last_name);
+            const fireBaseData = await signUp(formState.email, formState.password, lowerType);
             if (formState.type === 'trainer') {
                 await dispatch(postUser({...formState, type: lowerType, first_name: firstName, last_name: lastName, user_uid:fireBaseData.user.uid, last_login: Date.now()}))
                 await dispatch(postInviteCode({...inviteState, user_uid:fireBaseData.user.uid, invite_code: nanoid(5).toUpperCase()}));
