b44e291448c9066b98c57cf6df104229ef5aed2f
Louisa <louisawong95@hotmail.com>
2021-05-19 00:15:34 -0400

    style invite pages

--- fitome/pages/trainer/invitecode.js
+++ fitome/pages/trainer/invitecode.js
@@ -21,13 +21,16 @@ const Trainer = () => {
 
   
   return (
-    <div>
-      <h3>Start inviting your clients with this code</h3>
-        <p>{invite_code?.invite_code}</p>
+    <div className="initial_background">
+      <img className="initial_decor" src="/decor_background.png"/>
+      <img className="initial_wave" src="/wave.png"/>
+      <div className="signup_wrapper">
+        <div className="signup_code_title">Start inviting your clients with this code</div>
+        <p className="signup_code">{invite_code?.invite_code}</p>
         <a href="./profile">
-          <button className="button">Continue to Profile</button>
+          <button className="signup_button">Continue</button>
         </a>
-      <NavigationTrainer></NavigationTrainer>
+      </div>
     </div>
   )
 }
