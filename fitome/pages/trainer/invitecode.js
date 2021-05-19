14a64d4a78dcaf8e57ba2a60dde707eb9617f174
Louisa <louisawong95@hotmail.com>
2021-05-19 09:03:03 -0400

    styling profile and workout exercise

--- fitome/pages/trainer/invitecode.js
+++ fitome/pages/trainer/invitecode.js
@@ -4,11 +4,11 @@ import { getInviteCode, getUserById } from '../../redux/trainer';
 import { useAuth } from '../../firebase/contextAuth';
 import  { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import {useRouter} from 'next/router';
 
 const Trainer = () => {
   const dispatch = useDispatch();
-
+  const router = useRouter();
 
   const { currentUser } = useAuth();
   
@@ -25,10 +25,12 @@ const Trainer = () => {
       <img className="initial_decor" src="/decor_background.png"/>
       <img className="initial_wave" src="/wave.png"/>
       <div className="signup_wrapper">
+        <div className="signup_code_welcome">Welcome to</div>
+        <img className="initial_logo" src="/fitome_orange.png"/>
         <div className="signup_code_title">Start inviting your clients with this code</div>
         <p className="signup_code">{invite_code?.invite_code}</p>
         <a href="./profile">
-          <button className="signup_button">Continue</button>
+          <button className="signup_button" onClick={()=> router.push('/trainer/editprofile')}>Continue</button>
         </a>
       </div>
     </div>
